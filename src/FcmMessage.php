af92a9f05a75e76467979d83f33ea78a295879a7
Ahsaan Muhammad Yousuf <ahsaan@lntechnologies.com>
2017-12-13 17:17:44 +0500

    Made "to" method optional because of "condition" support

--- src/FcmMessage.php
+++ src/FcmMessage.php
@@ -243,7 +243,7 @@ class FcmMessage
 
         if (is_array($this->to)) {
             $payload['registration_ids'] = $this->to;
-        } else {
+        } elseif (!empty($this->to) ) {
             $payload['to'] = $this->to;
         }
 
