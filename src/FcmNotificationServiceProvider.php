c926e2b1bb9bbbe441b9530f9fbeb5bec466c16b
Gildas Tema <gildastema3@gmail.com>
2020-05-13 21:23:47 +0100

    fix resolve laravel7

--- src/FcmNotificationServiceProvider.php
+++ src/FcmNotificationServiceProvider.php
@@ -19,6 +19,11 @@ class FcmNotificationServiceProvider extends ServiceProvider
     {
         Notification::resolved(function (ChannelManager $service) {
             $service->extend('fcm', function () {
+                $version = app()->version();
+                $majorVersion = explode('.', $version)[0];
+                if( $majorVersion == 7){
+                    return new FcmChannel(new Client(), config('services.fcm.key'));
+                }
                 return new FcmChannel(app(Client::class), config('services.fcm.key'));
             });
         });
